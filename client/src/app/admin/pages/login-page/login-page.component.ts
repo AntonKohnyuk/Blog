c8d76acc09df174e27f8d6c2bc61c08029d92091
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2023-11-05 15:35:11 +0300

    Page: admin login page

--- client/src/app/admin/pages/login-page/login-page.component.ts
+++ client/src/app/admin/pages/login-page/login-page.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  public loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  submit() {
+    if (this.loginForm.invalid) return;
+    const user: User = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    };
+
+    this.loginForm.reset();
+  }
 }
