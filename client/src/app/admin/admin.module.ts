6e151d85f3e5bd9417b37b8047c64099ec4adce3
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2023-11-09 16:42:47 +0300

    Logic: start working with auth service

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './pages/create-page/create-page.component';
 import { EditPageComponent } from './pages/edit-page/edit-page.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
   declarations: [
@@ -16,5 +17,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     EditPageComponent,
   ],
   imports: [CommonModule, AdminRoutingModule, FormsModule, ReactiveFormsModule],
+  providers: [AuthService],
 })
 export class AdminModule {}
