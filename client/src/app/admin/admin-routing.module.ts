7421b556f510c660ce625197dc6b82fd7749e65e
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2023-11-25 20:50:31 +0300

    Logic: add working routing system with guards

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './pages/create-page/create-page.component';
 import { EditPageComponent } from './pages/edit-page/edit-page.component';
+import { authGuard } from './shared/services/auth.guard';
 
 const routes: Routes = [
   {
@@ -17,16 +18,19 @@ const routes: Routes = [
         path: 'dashboard',
         component: DashboardPageComponent,
         pathMatch: 'full',
+        canActivate: [authGuard],
       },
       {
         path: 'create',
         component: CreatePageComponent,
         pathMatch: 'full',
+        canActivate: [authGuard],
       },
       {
         path: 'post/:id/edit',
         component: EditPageComponent,
         pathMatch: 'full',
+        canActivate: [authGuard],
       },
       { path: '**', redirectTo: '/admin/dashboard' },
     ],
