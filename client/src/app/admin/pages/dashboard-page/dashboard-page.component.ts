4d98326e3546bda495be04d88b5a78ddae27948f
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2023-11-27 16:46:18 +0300

    Feature: view posts with filter
    add some fixes

--- client/src/app/admin/pages/dashboard-page/dashboard-page.component.ts
+++ client/src/app/admin/pages/dashboard-page/dashboard-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Sort } from '@angular/material/sort';
+import { Subscription } from 'rxjs';
 import { Post } from 'src/app/shared/interfaces/interfaces';
 import { PostsService } from 'src/app/shared/services/posts.service';
 
@@ -11,12 +12,14 @@ import { PostsService } from 'src/app/shared/services/posts.service';
 export class DashboardPageComponent implements OnInit, OnDestroy {
   public posts: Post[] = [];
   private postServiceSub!: Subscription;
-
+  public sortedData!: Post[];
+  public search = '';
   constructor(private postsServices: PostsService) {}
 
   ngOnInit(): void {
     this.postServiceSub = this.postsServices.getAll().subscribe((posts) => {
       this.posts = posts;
+      this.sortedData = this.posts.slice();
     });
   }
 
@@ -24,7 +27,31 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     if (this.postServiceSub) this.postServiceSub.unsubscribe();
   }
 
-  remove(postId: string) {
-    throw new Error('Method not implemented.');
+  remove(postId: string) {}
+
+  sortData(sort: Sort) {
+    const data = this.posts.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'author':
+          return compare(a.author, b.author, isAsc);
+        case 'title':
+          return compare(a.title, b.title, isAsc);
+        case 'date':
+          return compare(a.date.getTime(), b.date.getTime(), isAsc);
+        default:
+          return 0;
+      }
+    });
+
+    function compare(a: number | string, b: number | string, isAsc: boolean) {
+      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    }
   }
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { QuillModule } from 'ngx-quill';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [],
-  imports: [HttpClientModule, QuillModule.forRoot()],
-  exports: [HttpClientModule, QuillModule],
+  imports: [
+    HttpClientModule,
+    QuillModule.forRoot(),
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
+  exports: [
+    HttpClientModule,
+    QuillModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
 })
 export class SharedModule {}
