6735290bf86ad08ad5c221f608f5a0e3935cc658
Anton Kohnyuk <anton.kohnyuk@mail.ru>
2023-11-27 21:35:52 +0300

    Feature: add update post logic

--- client/src/app/admin/pages/edit-page/edit-page.component.ts
+++ client/src/app/admin/pages/edit-page/edit-page.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { PostsService } from 'src/app/shared/services/posts.service';
+import { Post } from 'src/app/shared/interfaces/interfaces';
+import { switchMap, Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.scss']
+  styleUrls: ['./edit-page.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
 })
-export class EditPageComponent {
+export class EditPageComponent implements OnInit, OnDestroy {
+  public post!: Post;
+  public isEdit: boolean = false;
+  public submitted: boolean = false;
+  public editForm!: FormGroup;
 
+  private postsServiceSubU!: Subscription;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.postsService.getPostById(params['id']);
+        })
+      )
+      .subscribe((post: Post) => {
+        this.post = post;
+        this.editForm = this.fb.group({
+          title: [
+            this.post.title,
+            [Validators.required, Validators.maxLength(40)],
+          ],
+          text: [this.post.text, Validators.required],
+        });
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.postsServiceSubU) this.postsServiceSubU.unsubscribe();
+  }
+
+  submit() {
+    if (this.editForm.invalid) return;
+
+    this.submitted = true;
+    let updatedPost = {
+      ...this.post,
+      title: this.editForm.value.title,
+      text: this.editForm.value.text,
+    };
+    this.postsServiceSubU = this.postsService
+      .update(updatedPost)
+      .subscribe(() => {
+        this.post = updatedPost;
+        this.submitted = false;
+        this.isEdit = !this.isEdit;
+      });
+  }
+
+  changeEdit() {
+    this.editForm.reset({ title: this.post.title, text: this.post.text });
+    this.isEdit = !this.isEdit;
+  }
 }
